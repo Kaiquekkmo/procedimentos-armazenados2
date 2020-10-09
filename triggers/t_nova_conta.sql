a11169723459b1eada88032eb01e56e7e59e7419
Kaiquekkmo <58341849+Kaiquekkmo@users.noreply.github.com>
2020-10-09 00:22:22 -0300

    Add files via upload

---
+++ triggers/t_nova_conta.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+DROP TRIGGER IF EXISTS t_nova_conta$$
+CREATE TRIGGER t_nova_conta BEFORE INSERT ON conta
+FOR EACH ROW
+BEGIN
+	IF (NEW.saldo < 0) THEN
+		SET NEW.saldo = 0;
+	END IF;
+    SET NEW.dtabertura_conta = CURRENT_TIMESTAMP ;
+END;
+$$
