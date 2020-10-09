89434f65c2a4b460c5506d2367b79157e561eb47
Kaiquekkmo <58341849+Kaiquekkmo@users.noreply.github.com>
2020-10-09 00:22:56 -0300

    Add files via upload

--- scripts/load-script.sql
+++ scripts/load-script.sql
@@ -2,34 +2,42 @@ DROP DATABASE IF EXISTS bancario;
 
 CREATE DATABASE bancario;
 
+DROP DATABASE IF EXISTS bancario;
+
+CREATE DATABASE bancario;
+
 USE bancario;
 
 CREATE TABLE conta (
-    numero_conta INT UNSIGNED,
-    nome_cliente VARCHAR(15) NOT NULL,
-    sobrenome_cliente VARCHAR(15) NOT NULL,
-    dtnasc_cliente DATE,
+    nr_conta SMALLINT(5) UNSIGNED auto_increment, /* Troquei o tipo da variavel para ocupar menos espaço no banco de dados, pois não há clientes que excedão 
+    um numero de conta equivalente a 65536, precisando de inúmeros cadastros, além do auto_increment  */
+    nm_cliente VARCHAR(15) NOT NULL,
+    snm_cliente VARCHAR(15) NOT NULL,
+    dt_nascimento DATE,
     dtabertura_conta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    saldo DECIMAL(10 , 2 ) NOT NULL DEFAULT 0,
-    PRIMARY KEY (numero_conta)
+    saldo DECIMAL(10 , 2) NOT NULL DEFAULT 0,
+    PRIMARY KEY (nr_conta)
 )  ENGINE=INNODB;
 
 CREATE TABLE movimentacao (
-    numero_conta INT UNSIGNED,
+    nr_conta SMALLINT(5) UNSIGNED,
     dtmov_conta TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'NÃO USAR ISSO NA PRÁTICA!',
-    valor_mov DECIMAL(10 , 2 ) NOT NULL,
-    descricao_mov VARCHAR(255),
-    PRIMARY KEY (numero_conta , dtmov_conta),
-    INDEX (numero_conta),
-    FOREIGN KEY (numero_conta)
-        REFERENCES conta (numero_conta)
+    vl_mov DECIMAL(10 , 2 ) NOT NULL,
+    ds_mov VARCHAR(55),
+    PRIMARY KEY (nr_conta , dtmov_conta),
+    INDEX (nr_conta),
+    FOREIGN KEY (nr_conta)
+        REFERENCES conta (nr_conta)
 )  ENGINE=INNODB;
 
-INSERT INTO conta VALUES(12345, 'João', 'Silva', '1995-12-25', null, 0);
-INSERT INTO conta VALUES(23497, 'Maria', 'Santos','1980-02-17', null, 0);
-INSERT INTO conta VALUES(876, 'Pedro', 'Ferreira','1943-10-20', null, 0);
-INSERT INTO conta VALUES(1236, 'Jéssica', 'Santos','1976-09-30', null, 0);
-INSERT INTO conta VALUES(9875, 'Stephani','Pereira', '1990-05-07', null, 0);
-INSERT INTO conta VALUES(6320, 'Paulo', 'Gomes','1987-03-24', null, 0);
-INSERT INTO conta VALUES(470, 'Augusto', 'Oliveira','1995-01-15', null, 0);
-INSERT INTO conta VALUES(987, 'Luana', 'Farias','1985-11-30', null, 0);
+INSERT INTO conta (nm_cliente,snm_cliente,dt_nascimento,dtabertura_conta, saldo)
+VALUES('João', 'Silva', '1995-12-25', default, 0),
+('Maria', 'Santos','1980-02-17', default, 0),
+('Maria', 'Santos','1980-02-17', default, 0),
+('Pedro', 'Ferreira','1943-10-20', default, 0),
+('Jéssica', 'Santos','1976-09-30', default, 0),
+('Stephani','Pereira', '1990-05-07', default, 0),
+('Paulo', 'Gomes','1987-03-24', default, 0),
+('Augusto', 'Oliveira','1995-01-15', default, 0),
+('Luana', 'Farias','1985-11-30', default, 0),
+('Leandro','Vanelli','2001-11-11',default,100);
